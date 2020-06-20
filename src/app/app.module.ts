ee6fa6cffe189d8765c50efc3c96527e390706c8
marion vutagwa <marionvutagwa@yahoo.com>
2020-06-20 13:35:58 +0300

    add search component and detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { SearchDetailComponent } from './search-detail/search-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    SearchDetailComponent
   ],
   imports: [
     BrowserModule,
