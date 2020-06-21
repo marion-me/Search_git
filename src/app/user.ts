8bd7b392f1a47b1b260ad6f6c780bd8b93356a53
marion vutagwa <marionvutagwa@yahoo.com>
2020-06-21 11:53:10 +0300

    add user component

---
+++ src/app/user.ts
@@ -0,0 +1,6 @@
+export class User {
+    constructor(public avatar_url: string, public name: string, public login: string,public email: string, public html_url: string,public public_repos: number,public followers: number,public  following: number,public created_at: Date){
+        
+    }
+
+ }
