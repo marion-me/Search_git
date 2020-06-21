83aab9c79976c993746604ae2c27455d9dde7c9e
marion vutagwa <marionvutagwa@yahoo.com>
2020-06-21 11:55:48 +0300

    add repo class to include the repository

---
+++ src/app/repo.ts
@@ -0,0 +1,6 @@
+export class Repo {
+        constructor(public name: string,public html_url: string,public description: string,public watchers: number,public forks: number,public created_at: Date){
+     
+        }
+         
+      }
