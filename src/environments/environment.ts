51edc6ef2392f07b3ef21e9f8eb2a679132883ba
marion vutagwa <marionvutagwa@yahoo.com>
2020-06-21 14:21:07 +0300

    add access_tocken to remove error

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  access_token:'6a0ebf76cdae7ddd8539f1d43b42b5aeb080ec8e'
 };
 
 /*
